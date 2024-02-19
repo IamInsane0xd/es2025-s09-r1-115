5cb1111b6fa2f7e14135adb9a597f81b91874521
biczoferenc <biczo.ferenc@icloud.com>
2024-02-19 15:41:00 +0100

    Display statistics and basic navbar
    There is a search button but the page is empty

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {StatsComponent} from "./stats/stats.component";
+import {NgIf} from "@angular/common";
+import {SearchComponent} from "./search/search.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StatsComponent, NgIf, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
   title = 'frontend';
+  currentPage = 0;
+
+  switchPage(page: number) {
+    this.currentPage = page;
+  }
 }
---
+++ frontend/src/app/stats/stats.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from "@angular/core";
+import {NgForOf, NgIf} from "@angular/common";
+
+type Stats = {
+  blockId: number,
+  capacity: number,
+  averageAge: number,
+  oldestContainerId: string,
+  newestContainerId: string,
+  emptyPositions: number,
+  emptyBays: number,
+  emptyStacks: number,
+}
+
+// noinspection ExceptionCaughtLocallyJS
+@Component({
+  selector: 'app-stats',
+  standalone: true,
+  imports: [
+    NgIf,
+    NgForOf,
+  ],
+  templateUrl: './stats.component.html',
+  styleUrl: './stats.component.less'
+})
+export class StatsComponent implements OnInit {
+  stats: Stats[] = []
+  errorMsg = ""
+  showErr = false
+
+  async ngOnInit(): Promise<void> {
+    this.showErr = false;
+
+    try {
+      const response = await fetch("http://localhost:3001/api/blocks/stat", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`api server returned with status ${response.status}`)
+      }
+
+      this.stats = (await response.json()) as Stats[];
+
+      if (this.stats == null) {
+        throw new Error("api responded with null")
+      }
+
+    } catch (e) {
+      this.showErr = true;
+
+      if (e instanceof Error) {
+        this.errorMsg = `error: ${e.message}`
+        return
+      }
+
+      this.errorMsg = `error: ${e}`
+      return
+    }
+  }
+}
